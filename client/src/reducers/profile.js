096cbeb0bf030c9a62c3c49f66b8220b865482b4
Ganesh <ganesh.bhogle88@gmail.com>
2021-02-23 20:07:01 -0500

    first commit

---
+++ client/src/reducers/profile.js
@@ -0,0 +1,58 @@
+import {
+  CLEAR_PROFIlE,
+  GET_PROFILE,
+  UPDATE_PROFIlE,
+  PROFILE_ERROR,
+  GET_PROFILES,
+  GET_REPOS,
+} from "../actions/types";
+
+const initialState = {
+  profile: null,
+  profiles: [],
+  repos: [],
+  loading: true,
+  error: {},
+};
+
+export default function (state = initialState, action) {
+  const { type, payload } = action;
+
+  switch (type) {
+    case GET_PROFILE:
+    case UPDATE_PROFIlE:
+      return {
+        ...state,
+        profile: payload,
+        loading: false,
+      };
+    case GET_PROFILES:
+      return {
+        ...state,
+        profiles: payload,
+        loading: false,
+      };
+    case PROFILE_ERROR:
+      return {
+        ...state,
+        error: payload,
+        loading: false,
+        profile: null,
+      };
+    case CLEAR_PROFIlE:
+      return {
+        ...state,
+        profile: null,
+        repos: [],
+        loading: false,
+      };
+    case GET_REPOS:
+      return {
+        ...state,
+        repos: payload,
+        loading: false,
+      };
+    default:
+      return state;
+  }
+}
